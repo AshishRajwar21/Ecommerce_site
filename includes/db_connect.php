f49ed4e2f59fefa330ba9e4cb06b8a405587c255
Ashish Rajwar <rajwarashish142@gmail.com>
2023-09-03 01:13:54 +0530

    db connection commit

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = mysqli_connect("127.0.0.1","root","","ecommerce");
+    //host,username,password,databasename
+    $conn = mysqli_connect("sql111.infinityfree.com","if0_34952357","4HV7Jy6dQVeS6","if0_34952357_ecomcart");
     if (mysqli_connect_errno()){
         echo "Connection Failed! ";
         return;
