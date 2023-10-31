51aa1a6e9906761dbf673e42c6048458e765ce03
Ashish Rajwar <rajwarashish142@gmail.com>
2023-11-01 00:04:19 +0530

    database change

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,6 +1,7 @@
 <?php
     //host,username,password,databasename
-    $conn = mysqli_connect("sql111.infinityfree.com","if0_34952357","4HV7Jy6dQVeS6","if0_34952357_ecomcart");
+    //$conn = mysqli_connect("sql111.infinityfree.com","if0_34952357","4HV7Jy6dQVeS6","if0_34952357_ecomcart");
+    $conn = mysqli_connect("localhost","root","","ecommerce");
     if (mysqli_connect_errno()){
         echo "Connection Failed! ";
         return;
